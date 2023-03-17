183dd8afd36934fb04e878dccdde47854d6aedaa
Wojciech Piętka <wojciech.pietka13@gmail.com>
2023-03-17 16:56:28 +0100

    Adjustments

--- src/display.c
+++ src/display.c
@@ -36,19 +36,19 @@ Tile* get_tile(uint16_t x, uint16_t y) {
     return display.map + (x * display.map_h) + y;
 }
 
-void Display_register_map(Tile *map, uint32_t width, uint32_t height, uint32_t scale) {
+void Display_register_map(Tile *map, uint32_t width, uint32_t height, uint32_t tile_size) {
     display.map = map;
     display.map_w = width;
     display.map_h = height;
-    int origin_x = (SCREEN_W / 2) - ((scale * display.map_w) / 2);
-    int origin_y = (SCREEN_H / 2) - ((scale * display.map_h) / 2);
+    int origin_x = (SCREEN_W / 2) - ((tile_size * display.map_w) / 2);
+    int origin_y = (SCREEN_H / 2) - ((tile_size * display.map_h) / 2);
     for (uint32_t i = 0; i < display.map_w; i++) {
         for (uint32_t j = 0; j < display.map_h; j++) {
             Tile *tile = get_tile(i, j);
-            tile->rectangle.w = scale - 5;
-            tile->rectangle.h = scale - 5;
-            tile->rectangle.x = (scale * i) + origin_x;
-            tile->rectangle.y = (scale * j) + origin_y;
+            tile->rectangle.w = tile_size - 5;
+            tile->rectangle.h = tile_size - 5;
+            tile->rectangle.x = (tile_size * i) + origin_x;
+            tile->rectangle.y = (tile_size * j) + origin_y;
         }
     }
 }
@@ -69,6 +69,15 @@ void draw_map() {
     }
 }
 
+void calculate_fps() {
+    static uint32_t fps_calculation_countdown = 0;
+    if (fps_calculation_countdown <= SDL_GetTicks()) {
+        printf("Current fps:%d\n", display.fps_counter);
+        fps_calculation_countdown = SDL_GetTicks() + 1000;
+        display.fps_counter = 0;
+    }
+}
+
 void Display_draw_frame() {
     static uint32_t next_frame_countdown = 0;
 
@@ -85,13 +94,4 @@ void Display_draw_frame() {
         display.fps_counter += 1;
     }
     calculate_fps();
-}
-
-void calculate_fps() {
-    static uint32_t fps_calculation_countdown = 0;
-    if (fps_calculation_countdown <= SDL_GetTicks()) {
-        printf("Current fps:%d\n", display.fps_counter);
-        fps_calculation_countdown = SDL_GetTicks() + 1000;
-        display.fps_counter = 0;
-    }
 }
--- src/display.h
+++ src/display.h
@@ -7,7 +7,7 @@
 
 uint8_t Display_initialize(const char* title);
 void Display_cleanup();
-void Display_register_map(Tile *map, uint32_t width, uint32_t height, uint32_t scale);
+void Display_register_map(Tile *map, uint32_t width, uint32_t height, uint32_t tile_size);
 void Display_draw_frame();
 
 #endif
